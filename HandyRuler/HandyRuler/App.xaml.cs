dee9400e5bf10d9e692a3fbc5d425565e498eee6
Геннадий <gemiyur@live.ru>
2026-02-14 23:07:12 +0300

    Создал решение HandyRuler.

---
+++ HandyRuler/HandyRuler/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HandyRuler
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
